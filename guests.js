ed9825f410ee183118412e81ddc1904fe6760f80
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-03-12 00:44:51 +0100

    feat pm on click on name + username promt

--- guests.js
+++ guests.js
@@ -5,8 +5,12 @@ var guests = {
   printList: function() {
     $('#list ul').html('');
     this.list.forEach(function(nick) {
-      $('#list ul').append('<li>' + nick + '</li>');
+      $('#list ul').append('<li class="guest"><div>' + nick + '</div></li>');
     })
+    $('#list li').bind('click', function(){
+      $('#text').val('@' + $(this).text() + ' ');
+      $('#text').focus();
+    });
   }
 
   }
